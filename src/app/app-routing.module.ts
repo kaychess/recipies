9b6bfc791f586dea75a2d437e263efc132d91dac
kaychess <kchsrinadh@gmail.com>
2019-06-17 21:08:23 -0400

    Created recipeDetailPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
   { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesPageModule' },
+  { path: 'recipe-detail', loadChildren: './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule' },
 ];
 
 @NgModule({
